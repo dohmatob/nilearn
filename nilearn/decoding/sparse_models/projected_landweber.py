621cb6135ad0fd767b3caa4310165a169ac48849
DOHMATOB Elvis Dopgima <elvis.dohmatob@inria.fr>
2014-07-04 13:29:15 +0200

    BF: using wrong norm (dual of) to compute duality gap

--- nilearn/decoding/sparse_models/projected_landweber.py
+++ nilearn/decoding/sparse_models/projected_landweber.py
@@ -6,13 +6,21 @@ from nilearn.decoding.sparse_models.common import (
     squared_loss_lipschitz_constant, gradient_id, div_id)
 
 
-def lp_norm(z, p):
-    # Compute lp norm of each 'atom'.
+def norm_lp(z, p):
+    """lp (mixed-)norm."""
+
     if hasattr(p, '__iter__'):
+        # handle mixed-norms
         if np.isinf(p[1]):
-            return np.max(lp_norm(z, p[0]), axis=0)
+            return np.max(norm_lp(z, p[0]), axis=0)
+        elif p[1] == 1.:
+            return np.sum(norm_lp(z, p[0]), axis=0)
+        elif p[1] == 2.:
+            aux = norm_lp(z, p[0])
+            return np.sqrt(np.sum(aux * aux, axis=0))
         else:
-            raise NotImplementedError("l_%s mixed-norm not implemented!" % p)
+            raise NotImplementedError(
+                "l_%s mixed-norm not implemented!" % str(p))
     elif p == 2.:
         return np.sqrt(np.sum(z * z, axis=0))
     elif p == 1.:
@@ -34,7 +42,7 @@ def proj_l2infty(y, kappa, inplace=True):
     out = y if inplace else np.array(y)
 
     # Do the actual projection (closed-form formula).
-    lp = lp_norm(y, 2.)
+    lp = norm_lp(y, 2.)
     nz = (lp > 0.)
     shrink = np.zeros_like(y)
     shrink[:, nz] = kappa / np.maximum(lp[nz][np.newaxis, :], kappa)
@@ -42,8 +50,8 @@ def proj_l2infty(y, kappa, inplace=True):
     return out
 
 
-def projected_landweber(A, AT, z, proj_p, p, lambd, stepsize, max_iter=100,
-                        dgap_tol=1e-4, verbose=1, align="", init=None,
+def projected_landweber(A, AT, z, proj_lp, norm_lp, lambd, stepsize, init=None,
+                        max_iter=100, dgap_tol=1e-4, verbose=1, align="",
                         return_info=False):
     if init is None:
         init = {}
@@ -62,9 +70,10 @@ def projected_landweber(A, AT, z, proj_p, p, lambd, stepsize, max_iter=100,
             break
         res = z - A(y)
         aux = AT(res)
-        dgap = lambd * lp_norm(aux, p) - np.dot(y, aux)
+        dgap = lambd * norm_lp(aux) - np.dot(y, aux)
+        assert dgap >= 0., dgap
         y += stepsize * aux  # foreward step
-        y = proj_p(y, lambd)  # backward step
+        y = proj_lp(y, lambd)  # backward step
     else:
         converged = False
 
@@ -77,8 +86,9 @@ def projected_landweber(A, AT, z, proj_p, p, lambd, stepsize, max_iter=100,
 class GradientId(object):
     def __init__(self, shape, l1_ratio):
         self.input_shape = shape
-        n_features = np.prod(shape)
-        self.shape = ((len(shape) + 1) * n_features, n_features)
+        self.n_features = np.prod(shape)
+        self.ndim = len(shape)
+        self.shape = ((self.ndim + 1) * self.n_features, self.n_features)
         self.l1_ratio = l1_ratio
         self.L = 1.1 * (4 * len(shape) * (1 - l1_ratio) ** 2 + l1_ratio ** 2)
 
@@ -90,10 +100,10 @@ class GradientId(object):
 
 class DivId(object):
     def __init__(self, shape, l1_ratio):
-        self.input_shape = [len(shape) + 1] + list(shape)
-        n_features = np.prod(shape)
-        self.shape = (n_features, (len(shape) + 1) * n_features)
-        self.shape = (n_features, (len(shape) + 1) * n_features)
+        self.ndim = len(shape)
+        self.input_shape = [self.ndim + 1] + list(shape)
+        self.n_features = np.prod(shape)
+        self.shape = (self.n_features, (self.ndim + 1) * self.n_features)
         self.l1_ratio = l1_ratio
         self.L = 1.1 * (4 * len(shape) * (1 - l1_ratio) ** 2 + l1_ratio ** 2)
 
@@ -105,12 +115,15 @@ class DivId(object):
 
 def prox_tv_l1(im, l1_ratio=.5, weight=50, dgap_tol=5.e-5, max_iter=10,
                verbose=False, init=None, return_info=False):
+    """TV-l1 prox by means of projected Landweber iterations."""
     D = GradientId(im.shape, l1_ratio)
     DT = DivId(im.shape, l1_ratio)
     proj = lambda w, kappa: proj_l2infty(w.reshape(
-            (len(D.input_shape) + 1, -1)), kappa).ravel()
-    out = projected_landweber(DT, D, im.reshape(D.shape[1]), proj,
-                              (2, np.inf), weight, 2. / D.L, init=init,
+            (D.ndim + 1, -1)), kappa).ravel()
+    norm = lambda w: norm_lp(w.reshape((D.ndim + 1, D.n_features)),
+                             (2, 1))
+    out = projected_landweber(DT, D, im.reshape(D.shape[1]), proj, norm,
+                              weight, 2. / D.L, init=init,
                               max_iter=max_iter, dgap_tol=dgap_tol,
                               verbose=verbose, return_info=return_info,
                               align="\t")
@@ -147,7 +160,7 @@ def test_proj_l2inty_not_outside_ball():
                                          np.logspace(-2, 2, num=6)):
         a = rng.randn(d, p)
         pa = proj_l2infty(a, kappa)
-        assert_true(lp_norm(pa, (2., np.inf)) <= kappa + salt)
+        assert_true(norm_lp(pa, (2., np.inf)) <= kappa + salt)
 
 
 if __name__ == '__main__':
@@ -156,6 +169,7 @@ if __name__ == '__main__':
     z = np.array([3., 4., 0, 4, 0])
     p = 2.
     lambd = 5.
-    proj_p = lambda y, lambd: proj_lp1(y.reshape((-1, 1)), lambd, p=p).ravel()
-    print projected_landweber(A, A.T(), z, proj_p, p, lambd, 2. / A.L,
+    proj = lambda y, lambd: proj_lp1(y.reshape((-1, 1)), lambd, p=p).ravel()
+    norm = lambda y: norm_lp(y, p)
+    print projected_landweber(A, A.T(), z, proj, norm, lambd, 2. / A.L,
                               align="\t")
